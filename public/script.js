1ca977ef28030ffbac94c7fa20f6be5f951787a1
Jennifer Gadelha <jhennygad3333@alu.ufc.br>
2023-07-15 12:25:32 -0300

    Script

---
+++ public/script.js
@@ -0,0 +1,9 @@
+let name = document.querySelector("#name")
+console.log(name)
+function buttonsubmit(e) {
+    event.preventDefault(e);
+    console.log("entrou");
+    e.target.reset();
+}
+
+document.querySelector()
