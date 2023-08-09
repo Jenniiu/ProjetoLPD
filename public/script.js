62fc6ad56048df8072fc0f475447bc4d5b49c5c7
Jennifer Gadelha <jhennygad3333@alu.ufc.br>
2023-08-08 22:04:57 -0300

    Forms

--- public/script.js
+++
@@ -1,9 +0,0 @@
-let name = document.querySelector("#name")
-console.log(name)
-function buttonsubmit(e) {
-    event.preventDefault(e);
-    console.log("entrou");
-    e.target.reset();
-}
-
-document.querySelector()
