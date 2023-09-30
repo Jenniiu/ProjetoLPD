1c1b2834d6490abf7e7aff18ed7f9f89a8efd24b
Jennifer Gadelha <jhennygad3333@alu.ufc.br>
2023-09-30 16:47:08 -0300

    Formulário

--- public/script.js
+++ public/script.js
@@ -1,27 +1,32 @@
-function sendEmail(){
-    console.log("entrou 123")
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const tel = document.getElementById("phone").value;
-    const message = document.getElementById("message").value;
-    const teste = {
-        name,
-        email,
-        tel,
-        message
-    }
-    console.log(teste)
+const fname = document.getElementById('fname').value;
+const email = document.getElementById('email').value;
+const phone = document.getElementById('phone').value;
+const message = document.getElementById('message').value;
+const submit = document.getElementById('form-contact');
+
+submit.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  console.log("teste");
+  
+  let ebody = `
+  <b>Nome: </b>${fname.value}
+  <br>
+  <b>Email: </b>${email.value}
+  <br>
+  <b>Telefone: </b>${phone.value}
+  <br>
+  <b>Mensagem: </b>${message.value}
+  
+  `
+  //email
   Email.send({
-      secureToken: "15bddccf-97c0-428f-954e-c2f5c6b6ce0e",
-      To : 'barbosajennifer00@gmail.com',
-      From : document.getElementById("email").value,
-      Subject : "New Contact Form Esquiry",
-      Body : "Name: " + document.getElementById("name").value
-      + "<br> Email: " + document.getElementById("email").value
-      + "<br> Telefone: " + document.getElementById("phone").value
-      + "<br> Message " + document.getElementById("message").value
-  }).then(
-    message => alert("Mensagem enviada com sucesso!")
-  );
-}
-sendEmail()
+    SecureToken : "828d3544-cda5-403b-8d1f-4af0dfa68efa",
+    To : 'changkiunmy@gmail.com',
+    From : "changkiunmy@gmail.com",
+    Subject : "Email teste " + email.value,
+    Body : ebody
+}).then(
+  message => alert(message)
+);
+});
+  
